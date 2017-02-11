93f41570a178ae885d60a5b36f1a702e11903348
francisyzy <you@example.com>
2017-02-11 15:02:35 +0800

    Website Fix
    Fix the naming of the logo file html5 validation error
    
    Added text to the showcase for the final project
    
    Added rudimentary side bar

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,12 @@
 function menuIcon(x) {
     x.classList.toggle("change");
 }
+function openNav() {
+	document.getElementById("Sidenav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
