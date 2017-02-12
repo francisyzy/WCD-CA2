37d5436c1d87110614714bd7da9f42d43857d01c
francisyzy <you@example.com>
2017-02-12 17:34:53 +0800

    Adjusted for mobile view

--- script/menu.js
+++ script/menu.js
@@ -1,12 +1,21 @@
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 function menuIcon(x) {
     x.classList.toggle("change");
+    sleep(500).then(() => {
+        x.classList.toggle("change");
+    });
+
 }
+
 function openNav() {
-	document.getElementById("Sidenav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("Sidenav").style.width = "250px";
+
 }
 
 function closeNav() {
     document.getElementById("Sidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
+
 }
